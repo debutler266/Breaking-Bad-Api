b5ac00e05e0c90595071e904d8c4848b2f126d52
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-12-01 16:27:31 -0500

    added components for characters and the header. Using hooks to load & set the api data

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 import Header from './components/ui/Header'
-import React from 'react'
+import CharacterGrid from './components/characters/CharacterGrid'
+
 
 function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+        console.log(result.data)
+
+        setItems(result.data)
+        setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [])
+
+
   return (
     <div className="container">
       <Header/>
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
