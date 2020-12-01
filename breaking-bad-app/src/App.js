ea24188260083e172e8239caa7856fdad35d44a8
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-12-01 16:54:07 -0500

    whatever is typed into search box now is passed through onchabge.. which takes event/e or whatever is typed & passes it through the state of text

--- breaking-bad-app/src/App.js
+++ breaking-bad-app/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import './App.css';
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
+      <Search/>
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
